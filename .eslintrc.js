a01309fbb1db26709e861e25226ca5a885c4491d
Justin Hall <justin.r.hall@gmail.com>
2022-04-09 14:35:37 -0600

    style(lint): silence eslint warning about React version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ['plugin:wkovacs64/base', 'prettier'],
+  settings: {
+    react: {
+      // config hack to work around eslint-plugin-react crying
+      version: '99999',
+    },
+  },
 };
