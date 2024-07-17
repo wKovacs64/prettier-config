4cbc1177143d9f5c2ad70da4280b186135d92fb6
Justin Hall <justin.r.hall@gmail.com>
2024-07-17 17:02:11 -0600

    style: replace eslint-plugin-wkovacs64 with @wkovacs64/eslint-config

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['plugin:wkovacs64/base', 'prettier'],
-  settings: {
-    react: {
-      // config hack to work around eslint-plugin-react crying
-      version: '99999',
-    },
-  },
-};
